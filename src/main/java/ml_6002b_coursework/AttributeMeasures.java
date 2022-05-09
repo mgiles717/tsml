3db9133fa9c67179508db2bf5d89818d88bb6c5f
mgiles717 <mgiles717@gmail.com>
2022-05-09 20:38:28 +0100

    Update AttributeMeasures.java

--- src/main/java/ml_6002b_coursework/AttributeMeasures.java
+++ src/main/java/ml_6002b_coursework/AttributeMeasures.java
@@ -1,10 +1,33 @@
 package ml_6002b_coursework;
 
 /**
- * Empty class for PArt 2.1 of the coursework
+ * Empty class for Part 2.1 of the coursework
  *
  */
 public class AttributeMeasures {
 
+    public static double measureInformationGain(){
+        return 0;
+    }
+
+    public static double measureInformationGainRatio(){
+        return 0;
+    }
+
+    public static double measureGini(){
+        return 0;
+    }
+
+    public static double measureChiSquared(){
+        return 0;
+    }
+
+    public static void main(String[] args){
+        System.out.println("Measure Information Gain for Peaty: " + measureInformationGain());
+        System.out.println("Measure Information Gain Ratio for Peaty: " + measureInformationGainRatio());
+        System.out.println("Measure Gini for Peaty: " + measureGini());
+        System.out.println("Measure Chi Squared for Peaty: " + measureChiSquared());
+
+    }
 
 }
