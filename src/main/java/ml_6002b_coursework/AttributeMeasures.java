7f035f36427803c5c1384d997731f9cf33b1cc74
mgiles717 <mgiles717@gmail.com>
2022-05-14 18:37:31 +0100

    Update AttributeMeasures.java

--- src/main/java/ml_6002b_coursework/AttributeMeasures.java
+++ src/main/java/ml_6002b_coursework/AttributeMeasures.java
@@ -6,21 +6,84 @@ package ml_6002b_coursework;
  */
 public class AttributeMeasures {
 
-    // Log base 10 to Log base 2 helper function
+    // Log base 10 to Log base 2 helper function, aimed to take fraction input e.g (4/5 or 0.8)
     public static double log2(double x){
         return Math.log(x)/Math.log(2);
+    }
+
+    // Calculate the total of a row in a table, takes array input
+    public static int tableSum(int[][] table){
+        int sum = 0;
+        for(int[] row : table){
+            for(int value: row){
+                sum += value;
+            }
+        }
+        return sum;
+    }
+
+    public static int rowSum(int[] row){
+        int sum = 0;
+        for (int value : row){
+            sum += value;
+        }
+        return sum;
+    }
+
+    public static int columnSum(int[][] table, int columnIndex){
+        int sum = 0;
+        for(int[] row : table){
+            sum += row[columnIndex];
+        }
+        return sum;
+    }
+
+    public static double rootEntropy(int[][] table){
+        int tableFrequency = tableSum(table);
+        double entropy = 0.0;
+        for (int[] row : table){
+            double proportion = (double)rowSum(row)/tableFrequency;
+            if (proportion == 1.0 || proportion == 0.0){
+                continue;
+            }
+            entropy += (proportion * log2(proportion));
+        }
+        return -entropy;
     }
 
     public static double infoGain(double x){
         return -((x * log2(x) + ((1-x)*log2(1-x))));
     }
+
     public static double measureInformationGain(int[][] table){
-        int[] rowTotal = new int[2];
+        int[] rowTotal = new int[table.length];
+        int tableFrequency = tableSum(table);
+        double infoGainValue = 0.0;
+
+        for(int i=0; i<table.length; i++){
+            rowTotal[i] = rowSum(table[i]);
+            for(int value: table[i]){
+                double proportion = (double)value/rowTotal[i];
+                //System.out.println("Value: " + value + ", RowTotal: " + rowTotal[i] + ", Proportion: " + proportion);
+
+                if (proportion == 1.0 || proportion == 0.0){
+                    continue;
+                }
+                infoGainValue += -((double)rowTotal[i]/tableFrequency)*(proportion * log2(proportion));
+            }
+        }
+        return rootEntropy(table) - (infoGainValue);
+    }
+    /*
+    public static double measureInformationGain(int[][] table){
+        int[] rowTotal = new int[table.length];
         int totalFrequency = 0, totalOccurrences = 0;
-        double[] infoGainValues = new double[2];
+        double[] infoGainValues = new double[table.length];
         for (int i=0; i<table.length; i++){
             // Take row total and calculate proportion for log calculation
-            rowTotal[i] = table[i][0] + table[i][1];
+            for (int j=0; j<table[i].length; j++){
+                rowTotal[i] += table[i][j];
+            }
             totalOccurrences += table[i][0];
             double proportion = (double) table[i][0]/rowTotal[i];
 
@@ -33,14 +96,15 @@ public class AttributeMeasures {
                 continue;
             }
             infoGainValues[i] = infoGain(proportion);
-            System.out.println(table[i][0] + ", " + table[i][1] + ", " + totalFrequency + ", tempval=" + proportion);
+            System.out.println(rowTotal[0]);
         }
         return infoGain((double)totalOccurrences/totalFrequency) - infoGainValues[0] - infoGainValues[1];
     }
+     */
 
     public static double measureInformationGainRatio(int[][] table){
         int totalFrequency = 0;
-        double[] splitInfoValues = new double[2];
+        double[] splitInfoValues = new double[table.length];
         double gainValue = measureInformationGain(table);
         for (int i=0; i<table.length; i++){
             splitInfoValues[i] = table[i][0];
@@ -62,9 +126,12 @@ public class AttributeMeasures {
     }
 
     public static void main(String[] args){
-        int[][] test_array = {{4,0},{1,5}};
-        //System.out.println("Measure Information Gain for Peaty: " + measureInformationGain(test_array));
-        System.out.println("Measure Information Gain Ratio for Peaty: " + measureInformationGainRatio(test_array));
+        int[][] peaty = {{4,0}, {1,5}};
+        int[][] test_array = {{4,1,2},{3,1,1},{1,3,4}};
+        //System.out.println(tableSum(test_array));
+        //System.out.println(rootEntropy(test_array));
+        System.out.println("Measure Information Gain for Peaty: " + measureInformationGain(test_array));
+        //System.out.println("Measure Information Gain Ratio for Peaty: " + measureInformationGainRatio(test_array));
         //System.out.println("Measure Chi Squared for Peaty: " + measureChiSquared());
         //System.out.println("Measure Gini for Peaty: " + measureGini());
 
